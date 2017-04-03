02954b6f5d6fb6a3e5a3f2c255620f78b74faf81
chris lloyd <dev@chrislloyd.me>
2017-04-03 19:52:34 -0400

    more

--- scripts/query.sql
+++
@@ -1,9 +0,0 @@
-SELECT value, count(*)
-FROM ways_tags
-JOIN (
-  SELECT DISTINCT id
-  FROM ways_tags
-  WHERE value = 'Renfrew'
-) w ON w.id = ways_tags.id
-GROUP BY value
-ORDER BY count(*) desc;
